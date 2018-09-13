2d46f74c24d55b9fa7b7b93580dfc9de8b840cbb
nao20010128nao <nao20010128@gmail.com>
2018-09-14 01:13:42 +0900

    BTCを追加

--- api.js
+++ api.js
@@ -25,5 +25,17 @@ create.MONA = create([
     "https://insight.electrum-mona.org/insight-api-monacoin",
     "https://mona.chainsight.info/api"
 ]);
+create.BTC = create([
+    "https://www.localbitcoinschain.com/api",
+    "https://blockexplorer.com/api",
+    "https://btc.coin.space/api",
+    "https://core.monacoin.ml/insight-api",
+    "https://explorer.bitcoin.com/api/btc",
+    "https://btc.blockdozer.com/insight-api",
+    "https://insight.bitpay.com/api",
+    "https://btc-bitcore4.trezor.io/api",
+    "https://btc-bitcore5.trezor.io/api",
+    "https://btc1.trezor.io/api"
+]);
 
 module.exports = create;

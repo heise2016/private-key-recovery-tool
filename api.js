1f451650734d4c5b563bd607b089ca1c53e8f424
nao20010128nao <nao20010128@gmail.com>
2018-09-16 22:07:41 +0900

    串対応

--- api.js
+++ api.js
@@ -1,21 +1,31 @@
 const axios = require("axios");
 const getExp = require("./coins").getExplorer;
 
+const proxies = [
+    a => a,
+    url => `https://zaif-status.herokuapp.com/proxy/${encodeURIComponent(url)}`
+];
+
 function create(endPoints) {
     let endPointOffset = 0;
+    let proxyOffset = 0;
     return async function request(path) {
         while (true) {
             try {
                 const data = (await axios({
-                    url: `${endPoints[endPointOffset]}${path}`,
-                    timeout: 1000
+                    url: proxies[proxyOffset](`${endPoints[endPointOffset]}${path}`),
+                    timeout: 1000,
+                    responseType: 'json'
                 })).data;
-                if (typeof data !== "object") {
+                if (typeof data !== "object" || data.error) {
                     throw data;
                 }
                 return data;
             } catch (e) {
-                endPointOffset = (endPointOffset + 1) % endPoints.length;
+                proxyOffset = (proxyOffset + 1) % proxies.length;
+                if (proxyOffset == 0) {
+                    endPointOffset = (endPointOffset + 1) % endPoints.length;
+                }
             }
         }
     }

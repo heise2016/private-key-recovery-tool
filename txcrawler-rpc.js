aa9f36280e6ede4fed156b9df763c39f682d6377
nao20010128nao <nao20010128@gmail.com>
2018-09-18 19:19:11 +0900

    切り出し

--- txcrawler-rpc.js
+++ txcrawler-rpc.js
@@ -1,13 +1,9 @@
 const path = require("path");
-const bitcoin = require("bitcoinjs-lib");
-const ec = require('elliptic').ec;
-const secp256k1 = new ec("secp256k1");
 const shell = require('shelljs');
 const leveldown = require('leveldown');
 const levelup = require('levelup');
 const api = require('./utils/api');
-const writePath = require("./utils/writepath");
-require("./utils/buffer-importder");
+const blockCracker = require("./utils/block-cracker");
 
 const cachePath = path.join(process.cwd(), "caches");
 try {
@@ -47,36 +43,7 @@ function normalFinish(a) {
             // The first one is coinbase, so skip
             const rawBlock = (await request(`/rawblock/${currentBlock}`)).rawblock;
             const rawBlockBuffer = Buffer.from(rawBlock, 'hex');
-            const loadedBlock = bitcoin.Block.fromBuffer(rawBlockBuffer);
-            let batch = db.batch();
-            for (let loaded of loadedBlock.transactions.slice(1)) {
-                const txId = loaded.getId();
-                const builder = bitcoin.TransactionBuilder.fromTransaction(loaded);
-                for (let vin in builder.inputs) {
-                    vin = parseInt(vin);
-                    const input = builder.inputs[vin];
-                    const input2 = loaded.ins[vin];
-                    console.log(`Writing down ${txId.slice(0,10)}... of input #${vin}; signatures: ${input.signatures.length}`)
-                    let signatureHash; // the message
-                    if (input.witness) {
-                        signatureHash = loaded.hashForWitnessV0(vin, input2.script, input.value, bitcoin.Transaction.SIGHASH_ALL);
-                    } else {
-                        signatureHash = loaded.hashForSignature(vin, input2.script, bitcoin.Transaction.SIGHASH_ALL);
-                    }
-                    for (let sig of input.signatures) {
-                        if (!Buffer.isBuffer(sig)) {
-                            continue;
-                        }
-                        sig.importDER();
-                        const pubKey = secp256k1.recoverPubKey(signatureHash, sig, 0).x.toBuffer();
-                        const dir = writePath(pubKey, sig, txId, vin);
-                        batch = batch
-                            .put(`${dir}/signature`, sig)
-                            .put(`${dir}/message`, signatureHash);
-                    }
-                }
-            }
-            await batch.write();
+            await db.batch(blockCracker(rawBlockBuffer));
         } else {
             console.log(`Skipped ${currentBlock} (#${blockInfo.height} ${progress}%)`);
         }
---
+++ utils/block-cracker.js
@@ -0,0 +1,46 @@
+const ec = require('elliptic').ec;
+const secp256k1 = new ec("secp256k1");
+const bitcoin = require("bitcoinjs-lib");
+const writePath = require("./utils/writepath");
+require("./utils/buffer-importder");
+
+module.exports = function (rawBlockBuffer) {
+    const batch = [];
+
+    const loadedBlock = bitcoin.Block.fromBuffer(rawBlockBuffer);
+    for (let loaded of loadedBlock.transactions.slice(1)) {
+        const txId = loaded.getId();
+        const builder = bitcoin.TransactionBuilder.fromTransaction(loaded);
+        for (let vin in builder.inputs) {
+            vin = parseInt(vin);
+            const input = builder.inputs[vin];
+            const input2 = loaded.ins[vin];
+            console.log(`Writing down ${txId.slice(0,10)}... of input #${vin}; signatures: ${input.signatures.length}`)
+            let signatureHash; // the message
+            if (input.witness) {
+                signatureHash = loaded.hashForWitnessV0(vin, input2.script, input.value, bitcoin.Transaction.SIGHASH_ALL);
+            } else {
+                signatureHash = loaded.hashForSignature(vin, input2.script, bitcoin.Transaction.SIGHASH_ALL);
+            }
+            for (let sig of input.signatures) {
+                if (!Buffer.isBuffer(sig)) {
+                    continue;
+                }
+                sig.importDER();
+                const pubKey = secp256k1.recoverPubKey(signatureHash, sig, 0).x.toBuffer();
+                const dir = writePath(pubKey, sig, txId, vin);
+                batch.push({
+                    type: 'put',
+                    key: `${dir}/signature`,
+                    value: sig
+                }, {
+                    type: 'put',
+                    key: `${dir}/message`,
+                    value: signatureHash
+                });
+            }
+        }
+    }
+
+    return batch;
+}
